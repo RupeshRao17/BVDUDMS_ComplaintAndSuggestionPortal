ceb4aadb32c4e5de3afcdc93e9201f14c225def5
Rupesh Rao <rupeshrao1709@gmail.com>
2025-01-15 12:55:19 +0530

    add deployment files

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJRVxf3caI8Lt2WFPS4Q1AXOOoNCGtMqQ",
-    authDomain: "college-complaint-suggestion.firebaseapp.com",
-    projectId: "college-complaint-suggestion",
-    storageBucket: "college-complaint-suggestion.firebasestorage.app",
-    messagingSenderId: "747067885712",
-    appId: "1:747067885712:web:b5358852d22471cc8df015"
-  };
+
+  apiKey: "AIzaSyBaluB-EFja0AEPQJoqI52X1CCsW7cOjyE",
+  authDomain: "college-complaint-suggestion.firebaseapp.com",
+  projectId: "college-complaint-suggestion",
+  storageBucket: "college-complaint-suggestion.firebasestorage.app",
+  messagingSenderId: "747067885712",
+  appId: "1:747067885712:web:3a5ae7606554a1e88df015",
+  measurementId: "G-778CTD0B8J"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
